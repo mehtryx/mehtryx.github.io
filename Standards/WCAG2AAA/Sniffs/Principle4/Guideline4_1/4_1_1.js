1053c2360b3bd7bea3a14c7cc8d14b2daf9f013c
Keith Benedict <kbenedict@postmedia.com>
2014-10-16 11:11:47 -0400

    testing nodename in message

--- Standards/WCAG2AAA/Sniffs/Principle4/Guideline4_1/4_1_1.js
+++ Standards/WCAG2AAA/Sniffs/Principle4/Guideline4_1/4_1_1.js
@@ -42,7 +42,7 @@ var HTMLCS_WCAG2AAA_Sniffs_Principle4_Guideline4_1_4_1_1 = {
 
             for (var i = 0; i < elsWithIds.length; i++) {
                 var id = elsWithIds[i].getAttribute('id');
-                var nodename = elsWithIds[i].nodeName.toLowercase(); 
+                var nodename = JSON.stringify( elsWithIds[i] ); 
                 if (usedIds[id] !== undefined) {
                     // F77 = "Failure of SC 4.1.1 due to duplicate values of type ID".
                     // Appropriate technique in HTML is H93.
